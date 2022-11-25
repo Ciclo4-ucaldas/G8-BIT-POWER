63ea2393f92325e6ae4033c414e30c9c9d4097bd
MagnusIskander <73327235+MagnusIskander@users.noreply.github.com>
2022-11-25 15:07:04 -0500

    build: add master page template an basic components: navbar, footer, sidebar, home and error

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './template/navbar/navbar.component';
+import { FooterComponent } from './template/footer/footer.component';
+import { SidebarComponent } from './template/sidebar/sidebar.component';
+import { HomeComponent } from './template/home/home.component';
+import { ErrorComponent } from './template/error/error.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    SidebarComponent,
+    HomeComponent,
+    ErrorComponent
   ],
   imports: [
     BrowserModule,
